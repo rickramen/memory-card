5d4b82467c5bd5f7610a0dec257835ae6ca8ae61
rickramen <91096615+rickramen@users.noreply.github.com>
2026-01-08 14:25:38 -0800

    cleanup template

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState, useEffect } from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <Header>
+        <h1>Memory Card Game</h1>
+      </Header>
+    </>
+  )
+}
+
+export default App
